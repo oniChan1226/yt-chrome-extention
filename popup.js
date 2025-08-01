550d3589f0a903f2771cf09e3a7967462fa0548d
oniChan1226 <ali943594@gmail.com>
2025-08-02 02:09:28 +0500

    add play button

--- popup.js
+++ popup.js
@@ -3,23 +3,29 @@ import { getCurrentTab } from "./utils.js";
 const addNewBookmark = (bookmarksElement, bookmark) => {
     const bookmarkTitleElement = document.createElement("div");
     const newBookmarkElement = document.createElement("div");
+    const controlElement = document.createElement("div");
 
     bookmarkTitleElement.textContent = bookmark.desc;
     bookmarkTitleElement.className = "bookmark-title";
+
+    controlElement.className = "bookmark-controls";
 
     newBookmarkElement.id = "bookmark-" + bookmark.time;
     newBookmarkElement.className = "bookmark";
     newBookmarkElement.setAttribute("timestamp", bookmark.time);
 
+    setBookmarkAttributes("play", onPlay, controlElement);
+
     newBookmarkElement.appendChild(bookmarkTitleElement);
+    newBookmarkElement.appendChild(controlElement);
     bookmarksElement.appendChild(newBookmarkElement);
 };
 
 const viewBookmarks = (currentBookmarks = []) => {
     const bookmarksElement = document.getElementById("bookmarks");
     bookmarksElement.innerHTML = "";
-    if(currentBookmarks.length > 0) {
-        for(let bookmark of currentBookmarks) {
+    if (currentBookmarks.length > 0) {
+        for (let bookmark of currentBookmarks) {
             addNewBookmark(bookmarksElement, bookmark);
         }
     } else {
@@ -27,11 +33,26 @@ const viewBookmarks = (currentBookmarks = []) => {
     }
 };
 
-const onPlay = e => {};
+const onPlay = async e => {
+    const bookmarkTime = e.target.parentNode.parentNode.getAttribute("timestamp");
+    const activeTab = await getCurrentTab();
 
-const onDelete = e => {};
+    chrome.tabs.sendMessage(activeTab.id, {
+        type: "PLAY",
+        value: bookmarkTime,
+    })
+};
 
-const setBookmarkAttributes =  () => {};
+const onDelete = e => { };
+
+const setBookmarkAttributes = (src, eventListener, controlParentElement) => {
+    const controlElement = document.createElement("img");
+
+    controlElement.src = `assets/${src}.png`;
+    controlElement.title = src;
+    controlElement.addEventListener("click", eventListener);
+    controlParentElement.appendChild(controlElement);
+};
 
 document.addEventListener("DOMContentLoaded", async () => {
 
@@ -42,8 +63,8 @@ document.addEventListener("DOMContentLoaded", async () => {
     const urlParameter = new URLSearchParams(queryParameters);
 
     const currentVideo = urlParameter.get("v");
-    
-    if(activeTab.url.includes("youtube.com/watch") && currentVideo) {
+
+    if (activeTab.url.includes("youtube.com/watch") && currentVideo) {
         chrome.storage.sync.get([currentVideo], (data) => {
             const currentVideoBookmarks = data[currentVideo] ? JSON.parse(data[currentVideo]) : [];
 
